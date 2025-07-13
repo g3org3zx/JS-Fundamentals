3b0d16f12811f4a269c48ee544c59e36c27962e1
George Kamande <161124335+g3org3zx@users.noreply.github.com>
2025-07-13 06:52:40 +0300

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,4 +1,7 @@
-
 const args = process.argv.slice(2);
 
-console.log(args[0] !== undefined ? args[0] : "No argument");
+if (args[0] === undefined) {
+  console.log("No argument");
+} else {
+  console.log(args[0]);
+}
